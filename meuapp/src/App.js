7a29d90f9fadf631e021010295933aa2d671195a
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-09-11 14:48:19 -0300

    Entendendo props e component

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,9 +1,20 @@
+import React from "react"
+
+const BemVindo = (props) => {
+  return (
+    <div>
+      <h2>Bem-vindo(a) {props.nome}</h2>
+      <h3>Você tem {props.idade} anos.</h3>
+    </div>
+  )
+}
 
 export default function App() {
   return (
     <div>
-      <h1>Bem-vindo ao Sistema!</h1>
-      <h2>@SujeitoProgramador :)</h2>
+      Olá Mundo!
+      <BemVindo nome='Misael' idade='30'/>
+      <BemVindo nome='Jheniffer' idade='24'/>
     </div>
   )
 }

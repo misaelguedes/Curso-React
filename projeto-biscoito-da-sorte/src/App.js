3fac88b0d48fad481ae20b35c222bca20351141f
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-09-13 14:45:59 -0300

    Troca de favicon e nova frase no Projeto Biscoito da Sorte.

--- projeto-biscoito-da-sorte/src/App.js
+++ projeto-biscoito-da-sorte/src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   const [textoFrase, setTextoFrase] = useState('')
 
   const [frases, setFrases] = useState([
-    'Siga os bons e aprenda com eles.', 'O riso é a menor distância entre duas pessoas.', 'Deixe de lado as preocupações e seja feliz.', 'Realize o óbvio, pense no improvável e conquiste o impossível.', 'Acredite em milagres, mas não dependa deles.', 'A maior barreira para o sucesso é o medo do fracasso.'
+    'Siga os bons e aprenda com eles.', 'O riso é a menor distância entre duas pessoas.', 'Deixe de lado as preocupações e seja feliz.', 'Realize o óbvio, pense no improvável e conquiste o impossível.', 'Acredite em milagres, mas não dependa deles.', 'A maior barreira para o sucesso é o medo do fracasso.', 'O bom-senso vale mais do que muito conhecimento.'
   ])
 
   function quebraBiscoito() {

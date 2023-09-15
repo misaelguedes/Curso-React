d17c0230f21380fe3291bc83edd68ca700a434a7
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-09-15 11:15:09 -0300

    Api useState

---
+++ meuapp8/src/App.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react"
+
+export default function App() {
+
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar React Hooks'
+  ])
+  const [input, setInput] = useState('')
+
+  function handleAdd() {
+    setTarefas([...tarefas, input])
+    setInput('')
+  }
+
+  return (
+    <div>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+
+      <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
+      <button type="button" onClick={handleAdd}>Adicionar</button>
+    </div>
+  )
+}

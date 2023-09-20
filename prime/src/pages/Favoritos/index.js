fa329c8822ec6f74f34da3fc253a11a570a258c5
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-09-20 12:05:40 -0300

    Manipulando lista de filmes

--- prime/src/pages/Favoritos/index.js
+++ prime/src/pages/Favoritos/index.js
@@ -12,9 +12,20 @@ export default function Favoritos() {
 
     }, [])
 
+    function excluirFilme(id) {
+        let filtroFilmes = filmes.filter((item) => {
+            return (item.id !== id)
+        })
+
+        setFilmes(filtroFilmes)
+        localStorage.setItem('@primeflix', JSON.stringify(filtroFilmes))
+    }
+
     return (
         <div className='meus-filmes'>
             <h1>Meus Filmes</h1>
+
+            {filmes.length === 0 && <span>Você não possui nenhum filme salvo!</span>}
 
             <ul>
                 {filmes.map((item) => {
@@ -23,7 +34,7 @@ export default function Favoritos() {
                             <span>{item.title}</span>
                             <div>
                                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                                <button>Excluir</button>
+                                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
                             </div>
                         </li>
                     )

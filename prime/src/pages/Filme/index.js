11ac6756ec222e7924b3f2271034b8ff05105137
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-09-20 10:25:00 -0300

    Detalhes do filme

--- prime/src/pages/Filme/index.js
+++ prime/src/pages/Filme/index.js
@@ -1,7 +1,54 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Api from '../../services/Api'
+
 export default function Filme() {
+
+    const {id} = useParams()
+    const [filme, setFilme] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        async function loadFilme() {
+            await Api.get(`/movie/${id}`, {
+                params: {
+                    api_key: '88c0c86971e06bf331ef2635f0edb21b',
+                    language: 'pt-BR'
+                }
+            })
+            .then((response) => {
+                setFilme(response.data)
+                setLoading(false)
+            })
+            .catch(() => {
+                console.log('Filme não encontrado!')
+            })
+        }
+
+        loadFilme()
+
+        return () => {
+            console.log('Componente desmontado!')
+        }
+    }, [])
+
+    if (loading) {
+        return (
+            <div className='filme-info'>
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            <h1>Bem-vindo à página Detalhes do Filme</h1>
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
         </div>
     )
 }

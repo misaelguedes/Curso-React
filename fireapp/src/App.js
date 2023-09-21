f2254f8a5e7aa5db0b637dba4692cc38ca43f4bc
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-09-21 15:03:42 -0300

    Trabalhando com Listas

--- fireapp/src/App.js
+++ fireapp/src/App.js
@@ -1,11 +1,13 @@
 import {useState} from 'react'
 import { db } from "./firebaseConnection"
-import {doc, setDoc, collection, addDoc, getDoc} from 'firebase/firestore'
+import {doc, setDoc, collection, addDoc, getDoc, getDocs} from 'firebase/firestore'
 import './app.css'
 
 export default function App() {
   const [titulo, setTitulo] = useState('')
   const [autor, setAutor] = useState('')
+
+  const [posts, setPosts] = useState([])
 
   async function cadastrar() {
   /* Cadastrar com ID específico
@@ -36,7 +38,8 @@ export default function App() {
   }
 
   async function buscarPost() {
-    const postRef = doc(db, 'posts', '12345')
+
+    /* const postRef = doc(db, 'posts', 'WxJwjzWDMWxoFpRW1pUo')
 
     await getDoc(postRef)
     .then((snapshot) => {
@@ -44,6 +47,26 @@ export default function App() {
       setTitulo(snapshot.data().Título)
     })
     .catch(() => {
+      console.log('ERRO AO BUSCAR!')
+    }) */
+
+    const postsRef = collection(db, 'posts')
+    await getDocs(postsRef)
+    .then((snapshot) => {
+      let lista = []
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          titulo: doc.data().Título,
+          autor: doc.data().Autor
+        })
+      })
+
+      setPosts(lista)
+
+    })
+    .catch((error) => {
       console.log('ERRO AO BUSCAR!')
     })
   }
@@ -61,6 +84,17 @@ export default function App() {
 
         <button onClick={cadastrar}>Cadastrar</button>
         <button onClick={buscarPost}>Buscar post</button>
+
+        <ul>
+          {posts.map((post) => {
+            return (
+              <li key={post.id}>
+                <span>Título: {post.titulo}</span> <br/>
+                <span>Autor: {post.autor}</span> <br/> <br/>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </div>
   )

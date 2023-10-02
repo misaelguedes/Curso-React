15147bb9a311428e47df08b106c0423f144a93ba
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-10-02 12:25:39 -0300

    Criando página Home

--- login/src/pages/Home/index.js
+++ login/src/pages/Home/index.js
@@ -1,9 +1,38 @@
+import {useState} from 'react'
+import {Link} from 'react-router-dom'
 import './Home.css'
 
 export default function Home() {
-    return (
-        <div>
 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleLogin(e) {
+        e.preventDefault()
+
+        if (email !== '' && password !== '') {
+            alert('Teste')
+        } else {
+            alert('Preencha todos os campos!')
+        }
+    }
+
+    return (
+        <div className='home-container'>
+            <h1>Lista de Tarefas</h1>
+            <span>Gerencie sua agenda de forma fácil.</span>
+
+            <form className='form' onSubmit={handleLogin}>
+                <input type='text' placeholder='Digite seu e-mail...' autoComplete='false' value={email} onChange={(e) => setEmail(e.target.value)}/>
+
+                <input type='password' placeholder='*****' value={password} onChange={(e) => setPassword(e.target.value)}/>
+
+                <button type='submit'>Acessar</button>
+            </form>
+
+            <Link to='/register' className='button-link'>
+                Não possui uma conta? Cadastre-se!
+            </Link>
         </div>
     )
 }

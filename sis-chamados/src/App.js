f7cc1289a5a4a981ccf9646658fc555c138d2e3e
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-10-10 12:47:50 -0300

    Criando página de cadastro

---
+++ sis-chamados/src/App.js
@@ -0,0 +1,11 @@
+import {BrowserRouter} from 'react-router-dom'
+
+import RoutesApp from './Routes'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <RoutesApp/>
+    </BrowserRouter>
+  )
+}
---
+++ sis-chamados/src/Routes/index.js
@@ -0,0 +1,16 @@
+import {Routes, Route} from 'react-router-dom'
+
+import SignIn from '../pages/SignIn'
+import SignUp from '../pages/SingUp'
+import Dashboard from '../pages/Dashboard'
+
+export default function RoutesApp() {
+    return (
+        <Routes>
+            <Route path='/' element={<SignIn/>}/>
+            <Route path='/register' element={<SignUp/>}/>
+            
+            <Route path='/dashboard' element={<Dashboard/>}/>
+        </Routes>
+    )
+}

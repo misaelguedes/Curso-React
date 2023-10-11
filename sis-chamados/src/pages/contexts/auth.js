af812a8f44cdbdc639f7b27dab550e19f6827b6f
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-10-11 17:11:45 -0300

    Logando usuário

--- sis-chamados/src/pages/contexts/auth.js
+++ sis-chamados/src/pages/contexts/auth.js
@@ -1,6 +1,6 @@
 import {useState, useEffect, createContext, Children} from 'react'
 import {auth, db} from '../../Services/firebaseConnection'
-import {createUserWithEmailAndPassword} from 'firebase/auth'
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth'
 import {doc, getDoc, setDoc} from 'firebase/firestore'
 import {useNavigate} from 'react-router-dom'
 import {toast} from 'react-toastify'
@@ -14,8 +14,34 @@ export default function AuthProvider({children}) {
 
     const navigate = useNavigate()
 
-    function signIn(email, password) {
+    async function signIn(email, password) {
+        setLoadingAuth(true)
 
+        await signInWithEmailAndPassword(auth, email, password)
+        .then( async (value) => {
+            let uid = value.user.uid
+
+            const docRef = doc(db, 'users', uid)
+            const docSnap = await getDoc(docRef)
+
+            let data = {
+                uid: uid,
+                nome: docSnap.data().nome,
+                email: value.user.email,
+                avatarUrl: docSnap.data().avatarUrl
+            }
+
+            setUser(data)
+            storageUser(data)
+            setLoadingAuth(false)
+            toast.success('Bem-vindo(a) de volta!')
+            navigate('/dashboard')
+        })
+        .catch((error) => {
+            console.log(error)
+            setLoadingAuth(false)
+            toast.error('Algo deu errado!')
+        })
     }
 
     // Cadastrar novo usuário

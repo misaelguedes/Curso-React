14b8b790903c7c1d4c68b2b63cd7b24c179a2904
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-10-13 11:28:50 -0300

    Criando header

--- sis-chamados/src/pages/Dashboard/index.js
+++ sis-chamados/src/pages/Dashboard/index.js
@@ -1,6 +1,8 @@
 import { AuthContext } from "../contexts/auth"
 import { useContext } from "react"
 
+import Header from "../../components/Header/indx"
+
 export default function Dashboard() {
 
     const {logout} = useContext(AuthContext)
@@ -11,6 +13,8 @@ export default function Dashboard() {
 
     return (
         <div>
+            <Header/>
+
             <h1>Página Dashboard</h1>
             <button onClick={handleLogout}>Sair da conta</button>
         </div>

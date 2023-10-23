ea958b94d3754c840632df1d5f716e4039814c9b
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-10-23 14:02:52 -0300

    Editar chamado

--- sis-chamados/src/pages/New/index.js
+++ sis-chamados/src/pages/New/index.js
@@ -9,6 +9,8 @@ import { AuthContext } from '../contexts/auth'
 import { db } from '../../Services/firebaseConnection'
 import {collection, getDocs, getDoc, doc, addDoc} from 'firebase/firestore'
 
+import {useParams} from 'react-router-dom'
+
 import {toast} from 'react-toastify'
 
 const listRef = collection(db, 'customers')
@@ -16,6 +18,7 @@ const listRef = collection(db, 'customers')
 export default function New() {
 
     const {user} = useContext(AuthContext)
+    const {id} = useParams()
 
     const [customers, setCustomers] = useState([])
     const [loadCustomer, setLoadCustomer] = useState(true)
@@ -24,6 +27,7 @@ export default function New() {
     const [complemento, setComplemento] = useState('')
     const [assunto, setAssunto] = useState('Suporte')
     const [status, setStatus] = useState('Aberto')
+    const [idCustomer, setIdCustomer] = useState(false)
 
     useEffect(() => {
         async function loadCustomers() {
@@ -48,6 +52,10 @@ export default function New() {
                 setCustomers(lista)
                 setLoadCustomer(false)
 
+                if (id) {
+                    loadId(lista)
+                }
+
             })
             .catch((error) => {
                 console.log('ERRO AO BUSCAR OS CLIENTES', error)
@@ -57,7 +65,26 @@ export default function New() {
         }
 
         loadCustomers()
-    }, [])
+    }, [id])
+
+    async function loadId(lista) {
+        const docRef = doc(db, 'chamados', id)
+        await getDoc(docRef)
+        .then((snapshot) => {
+            setAssunto(snapshot.data().assunto)
+            setStatus(snapshot.data().status)
+            setComplemento(snapshot.data().complemento)
+
+            let index = lista.fidIndex(item => item.id === snapshot.data().clienteId)
+            setCustomerSelected(index)
+            setIdCustomer(true)
+
+        })
+        .catch((error) => {
+            console.log(error)
+            setIdCustomer(false)
+        })
+    }
 
     function handleOptionChange(e) {
         setStatus(e.target.value)
@@ -73,6 +100,11 @@ export default function New() {
 
     async function handleRegister(e) {
         e.preventDefault()
+
+        if (idCustomer) {
+            alert('EDITANDO CHAMADO')
+            return
+        }
 
         //Registrar chamado
         await addDoc(collection(db, 'chamados'), {
